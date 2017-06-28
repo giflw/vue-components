3cb5de8e3fcd8825478d5dbc82ef9706214c00f7
Guilherme I F L Weizenmann <guilherme@itquasar.com>
2017-06-28 00:08:47 -0300

    Use object notation on entry config in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'giflw-vue-components': './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'giflw-vue-components.js',
+    filename: '[name].bundle.js',
     library: 'GIFLWVueComponents',
     libraryTarget: 'umd'
   },
